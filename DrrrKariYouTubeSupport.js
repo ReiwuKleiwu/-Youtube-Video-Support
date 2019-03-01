623a7d08be9bb4e1e78fd3697247e9c0996647f9
ReiwuKleiwu <xxstruckixx@gmail.com>
2019-03-01 16:15:41 +0100

    Add files via upload

---
+++ DrrrKariYouTubeSupport.js
@@ -0,0 +1,45 @@
+"use strict";
+
+window.addEventListener("load", function () {
+
+            setInterval(function () {
+
+                let talks = document.getElementById("talks");
+
+                for (let i = 0; i < talks.children.length; i++) {
+                    if (talks.children[i].children[1] !== undefined) {
+                        let msg = talks.children[i].children[1].children[0].children[1].innerHTML;
+
+                        if(msg.includes("www.youtube.com/watch")){
+
+                            let msgBubble = talks.children[i].children[1].children[0].children[1];
+
+                            msgBubble.style.height = '';
+                            msgBubble.style.width = '';
+
+                            let YTURL = msgBubble.children[0].innerHTML;
+
+                            YTURL = YTURL.replace('watch?v=', "embed/");
+
+                            let iFrame = document.createElement("iFrame");
+                            iFrame.style.width = "560px";
+                            iFrame.style.height = "315px";
+                            iFrame.style.border = "2px solid black";
+                            iFrame.style.borderRadius ="5px";
+                            iFrame.src = YTURL;
+
+                            if(msgBubble.className !== 'body YOUTUBE'){
+                                msgBubble.classList.add('YOUTUBE');
+                                msgBubble.appendChild(iFrame);
+                            }
+
+
+                        }
+                    }
+                }
+
+
+            }, 1000);
+
+
+        }, false);
